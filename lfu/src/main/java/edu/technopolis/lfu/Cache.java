abde40f764ae421cf5a1989464c452c761554e63
Anton Lamtev <antonlamtev@gmail.com>
2017-01-14 00:17:08 +0300

    template

--- lfu/src/main/java/edu/technopolis/lfu/Cache.java
+++ lfu/src/main/java/edu/technopolis/lfu/Cache.java
@@ -1,4 +1,7 @@
 package edu.technopolis.lfu;
 
-public interface Cache {
+public interface Cache<K, V> {
+    void put(K key, V value);
+    V get(K key);
+    int size();
 }
--- lfu/src/main/java/edu/technopolis/lfu/LFUCache.java
+++ lfu/src/main/java/edu/technopolis/lfu/LFUCache.java
@@ -1,4 +1,35 @@
 package edu.technopolis.lfu;
 
-public class LFUCache {
+import java.util.HashMap;
+import java.util.Map;
+
+public class LFUCache<K, V> implements Cache<K, V> {
+
+    private static final int DEFAULT_CAPACITY = 10;
+    private final int CAPACITY;
+    private Map<K, V> cache;
+
+    public LFUCache(int capacity) {
+        CAPACITY = capacity;
+        cache = new HashMap<>(capacity);
+    }
+
+    public LFUCache() {
+        this(DEFAULT_CAPACITY);
+    }
+
+    @Override
+    public void put(K key, V value) {
+        //TODO
+    }
+
+    @Override
+    public V get(K key) {
+        return cache.get(key);
+    }
+
+    @Override
+    public int size() {
+        return cache.size();
+    }
 }
